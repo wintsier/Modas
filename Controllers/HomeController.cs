3615c089d0b9551c3b47355fc6d5f01ff88eb0fa
wintsier <sierrawinter@earthlink.net>
2020-01-20 19:26:15 -0600

    Configure MVC application with default route

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Modas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() => "This is POCO";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Modas2
@@ -15,6 +14,7 @@ namespace Modas2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +25,7 @@ namespace Modas2
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
