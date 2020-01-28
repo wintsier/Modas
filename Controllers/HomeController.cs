e2db5dada3320cfbabfee28bbef92cde48b7ea98
wintsier <sierrawinter@earthlink.net>
2020-01-27 19:20:10 -0600

    Lazy loading, Eager loading, Pagination

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Modas.Models;
+using Modas.Models.ViewModels;
+using System.Linq;
 
 namespace Modas.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly int PageSize = 20;
         private IEventRepository repository;
         public HomeController(IEventRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult Index() => View(repository.Events);
+        public ViewResult Index(int page = 1) => View(new EventListViewModel
+        {
+            Events = repository.Events
+                .Include(e => e.Location)
+                .OrderByDescending(e => e.TimeStamp)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Events.Count()
+            }
+        });
     }
 }
