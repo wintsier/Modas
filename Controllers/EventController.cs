2b972ce96a77135d59a891105f3be88c87f60610
wintsier <sierrawinter@earthlink.net>
2020-02-17 20:03:27 -0600

    API View Model

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,15 +1,16 @@
-
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Modas.Models;
+using Modas.Models.ViewModels;
 
 namespace Modas.Controllers
 {
     [Route("api/[controller]")]
     public class EventController : Controller
     {
+        private readonly int PageSize = 20;
         private IEventRepository repository;
         public EventController(IEventRepository repo) => repository = repo;
 
@@ -17,6 +18,30 @@ namespace Modas.Controllers
         // returns all events (unsorted)
         public IEnumerable<Event> Get() => repository.Events
             .Include(e => e.Location);
+
+        [HttpGet("page{page:int}")]
+        // returns all events by page
+        public ApiListViewModel GetPage(int page = 1) =>
+            new ApiListViewModel
+            {
+                Events = repository.Events
+                    .Select(e => new ApiViewEvent
+                    {
+                        EventId = e.EventId,
+                        Flagged = e.Flagged,
+                        TimeStamp = e.TimeStamp,
+                        LocationName = e.Location.Name
+                    })
+                    .OrderByDescending(e => e.TimeStamp)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Events.Count()
+                }
+            };
 
         [HttpGet("{id}")]
         // return specific event
